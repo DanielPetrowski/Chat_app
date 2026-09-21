0bdebc3b04fe9a7f298c7862b8faff6674e442ef
DanielPetrowski <petrowskidaniel@web.de>
2026-09-21 12:36:19 +0200

    feat: add chat view for get and post

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Chat
+import json
 
-# Create your views here.
+def chat_view(request):
+   
+    if request.method == 'GET':
+        nachrichten = list(Chat.objects.all().values())
+        return JsonResponse(nachrichten, safe=False)
+        
+    
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        Chat.objects.create(
+            name=data['name'],
+            message=data['message']
+        )
+        return JsonResponse({'status': 'ok'})
